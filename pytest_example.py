83aa30f4ffcc4b10b4a0876fcea634e3e2dbb6e0
mattiaguerri <mattguerri1@gmail.com>
2018-10-19 11:44:15 +0100

    edit_1

--- pytest_example.py
+++ pytest_example.py
@@ -2,6 +2,6 @@ import foo
 
 def test_solve_quadratic():
     
-    assert(foo.solve_quadratic(1,0,-1)==(-1.0,5.0))
+    assert(foo.solve_quadratic(1,0,-1)==(-1.0,1.0))
     assert(foo.solve_quadratic(1,0,0)==0.0)
     assert(foo.solve_quadratic(1,0,1) is None)
